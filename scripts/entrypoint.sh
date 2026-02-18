41b906bdfff35da81885bd6272b2738dbe71eab7
2685147601lsl-ops <2685147601lsl@gmail.com>
2026-02-18 21:24:08 +0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-set -eu
-
-/app/scripts/init_storage.sh
-
-exec "$@"
+#!/usr/bin/env sh  
+set -eu  
+/app/scripts/init_storage.sh  
+exec uvicorn main:app --host 0.0.0.0 --port 8080  
